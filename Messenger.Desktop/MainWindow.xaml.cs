1f3a4b8a8269fe7c983e5d77397ab9d92e6f66fa
KupuJIJI12 <kirillkruglov2013ru@mail.ru>
2021-02-14 01:47:44 +0400

    Каркас + Страница диалога

--- Messenger.Desktop/MainWindow.xaml.cs
+++ Messenger.Desktop/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Messenger.Database;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Messenger.Database;
 using Messenger.WebApi;
 
 namespace Messenger.Desktop
@@ -14,6 +17,13 @@ namespace Messenger.Desktop
             // эти две строчки просто для теста, что все будет работать без ошибок, удалите, когда будете делать
             //var db = new DatabaseContext();
             var api = new Api();
+            frame.NavigationService.Navigate(new Uri("Pages/PageNoDialog.xaml", UriKind.Relative));
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            frame.Source = null;
+            frame.Navigate(new Uri("Pages/Dialog.xaml", UriKind.Relative));
         }
     }
 }

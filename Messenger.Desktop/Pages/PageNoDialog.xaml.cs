096e139f5bfd920580dc3de6ee96dc46255a1784
KupuJIJI12 <kirillkruglov2013ru@mail.ru>
2021-02-14 14:56:23 +0400

    Commit2

---
+++ Messenger.Desktop/Pages/PageNoDialog.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Messenger.Desktop.Pages
+{
+    public partial class PageNoDialog : Page
+    {
+        public PageNoDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}

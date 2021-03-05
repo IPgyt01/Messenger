6ca0aa3defe9ec22c2013cc569916a4b0eba82c9
shtankov.leha@mail.ru <shtankov.leha@mail.ru>
2021-03-05 20:44:38 +0400

    страницы авторизации и регистрации

---
+++ Messenger.Desktop/Pages/Authorization.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Messenger.Desktop.Pages
+{
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Messenger.Desktop/Pages/Registration.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Messenger.Desktop.Pages
+{
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+    }
+}
